6a0a533ca8fe18d81ac9f6bea11de7b9dd39f3d3
Paul Schneider <paulschneider@free.fr>
2015-05-12 11:35:54 +0200

    Télécommande freebox
    Un clavier de télécommande pour le boitier Haute-Définition n°1 de Free.

---
+++ freetelec.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace freetelec
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new MyPage();
+
+			// power list tv
+			// 0-9
+			// back swap
+			// info mail help pip
+			// epg media options
+			// vol_inc vol_dec
+			// prgm_inc prgm_dec
+			// ok
+			// up right down left
+			// mute home rec bwd (ret arr)
+			// prev play fwd
+			// next
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
